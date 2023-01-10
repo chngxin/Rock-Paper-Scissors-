4150fc59495eca3b90748cb6f9bbdd814f80048a
Ch'ng Xin <chngxin96@gmail.com>
2023-01-10 14:24:00 +0800

    Completed html boilerplate and added Javascript that prompts an input for the player.

---
+++ myscript.js
@@ -0,0 +1,35 @@
+const choices = ["rock", "paper", "scissors"] 
+
+function game() {
+    playRound();
+}
+
+function playRound() {
+    const playerSelection = playerChoice()
+    const computerSelection = computerChoice()
+}
+
+function playerChoice() {
+ let input = prompt("Type Rock, Paper, or Scissors"); 
+ while (input == null) {
+    input = prompt("Type Rock, Paper, or Scissors"); 
+ }
+ input = input.toLowerCase();
+ let check = validateInput(input);
+ while (check == false) {
+    input = prompt("Try again. Please Type Rock, Paper, or Scissors");
+    input = input.toLowerCase();
+    check = validateInput(input);
+ }
+ // console.log(input)
+}
+
+function computerChoice() {
+return choices[Math.floor(Math.random()*choices.length)]
+}
+
+function validateInput(choice) {
+return choices.includes(choice); 
+}
+
+game ();
