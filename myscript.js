b28faf38497a66729621d1637056449ca316791f
Ch'ng Xin <chngxin96@gmail.com>
2023-01-11 17:42:28 +0800

    Made sure the game has 5 rounds and selected a winner.

--- myscript.js
+++ myscript.js
@@ -1,12 +1,18 @@
 const choices = ["rock", "paper", "scissors"] 
+const winners = [];
 
 function game() {
+   for (let i = 0; i <= 5; i++){
     playRound();
-}
+   }
+   logWins();
+}   
 
 function playRound() {
     const playerSelection = playerChoice()
     const computerSelection = computerChoice()
+    const winner = checkWinner(playerSelection, computerSelection)
+    winners.push(winner)
 }
 
 function playerChoice() {
@@ -18,10 +24,13 @@ function playerChoice() {
  let check = validateInput(input);
  while (check == false) {
     input = prompt("Try again. Please Type Rock, Paper, or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors"); 
+     }
     input = input.toLowerCase();
     check = validateInput(input);
  }
- // console.log(input)
+ return input;
 }
 
 function computerChoice() {
@@ -30,6 +39,21 @@ return choices[Math.floor(Math.random()*choices.length)]
 
 function validateInput(choice) {
 return choices.includes(choice); 
+}
+
+function checkWinner(choiceP, choiceC){
+    if(choiceP === choiceC){
+    return "Tie";
+    } else if ((choiceP === "rock" && choiceC === "scissors") || (choiceP === "paper" && choiceC === "rock") || (choiceP === "scissors" && choiceC === "rock")){
+        return "Player";
+    } else {
+        return "Computer";
+    }
+
+}
+
+function logWins() {
+    console.log(winners);
 }
 
 game ();
